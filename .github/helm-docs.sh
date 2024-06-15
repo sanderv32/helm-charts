3c194f0da879000396e38f59e4e69f8e61492294
Alexander <sanderv32@gmail.com>
2024-06-15 22:34:11 +0200

    Fix for local use

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -2,11 +2,19 @@
 set -euo pipefail
 
 HELM_DOCS_VERSION="1.13.1"
+ARCH=$(uname -m)
 
-# install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
-tar -xf /tmp/helm-docs.tar.gz helm-docs
+if which helm-docs > /dev/null; then
+    echo "helm-docs is already installed"
 
-# validate docs
-./helm-docs
+    helm-docs
+else
+    # install helm-docs
+    curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_${ARCH}.tar.gz
+    tar -xf /tmp/helm-docs.tar.gz helm-docs
+
+    # validate docs
+    ./helm-docs
+fi
+
 git diff --exit-code
